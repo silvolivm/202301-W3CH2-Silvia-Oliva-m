ebf33569c0c3a20ec85830157bec8f5293d3be67
silvia <silvolivm@gmail.com>
2023-02-01 23:10:53 +0100

    add mocks

---
+++ src/__mocks__/assetsMocks.ts
@@ -0,0 +1,7 @@
+import { Pet } from '../models/pet';
+
+export const assetsMocks: Pet[] = [
+  new Pet('Braco', 'Carea Leones'),
+  new Pet('Thor', ' Gran danes', 'Paloma'),
+  new Pet('Rodo', 'Pastor Aleman', 'Paco'),
+];
